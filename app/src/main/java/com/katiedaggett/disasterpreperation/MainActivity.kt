2467825650020d64030d5eb8b734cd1f93af2182
katherin97 <60371103+katherin97@users.noreply.github.com>
2020-04-08 10:55:35 -0500

    2020.04.08

--- app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
+++ app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
@@ -2,11 +2,34 @@ package com.katiedaggett.disasterpreperation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstVal = 1
+        val finalNumber = firstNumber(firstVal)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "The number is $finalNumber"
     }
+
+    fun firstNumber(num: Int): Int {
+        var finalNumb = num
+        var first = 3
+        var second = 5
+        var final = 0
+
+        while (finalNumb >= 0) {
+            final = first * second
+            finalNumb--
+        }
+        return final
+    }
+
+
+
 }
