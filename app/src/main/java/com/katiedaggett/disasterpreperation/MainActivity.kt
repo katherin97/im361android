9af10abda73f2caec6c65daa32f4bdf266dc2d0b
katherin97 <60371103+katherin97@users.noreply.github.com>
2020-04-13 09:53:28 -0500

    2020.04.13

--- app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
+++ app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
@@ -10,26 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstVal = 1
-        val finalNumber = firstNumber(firstVal)
 
-        val textView = findViewById<TextView>(R.id.text_view)
-        textView.text = "The number is $finalNumber"
+        //val textView = findViewById<TextView>(R.id.text_view)
     }
-
-    fun firstNumber(num: Int): Int {
-        var finalNumb = num
-        var first = 3
-        var second = 5
-        var final = 0
-
-        while (finalNumb >= 0) {
-            final = first * second
-            finalNumb--
-        }
-        return final
-    }
-
-
 
 }
