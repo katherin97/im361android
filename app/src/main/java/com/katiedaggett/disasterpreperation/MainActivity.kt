46c6ea93583d66441a5683fa6fd33d65e666b947
katherin97 <60371103+katherin97@users.noreply.github.com>
2020-04-21 21:00:18 -0500

    2020.04.22

--- app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
+++ app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
@@ -3,15 +3,32 @@ package com.katiedaggett.disasterpreperation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewPager = findViewById(R.id.view_pager)
 
-        //val textView = findViewById<TextView>(R.id.text_view)
+        val adapter = MainActivityPagerAdapter(this)
+        viewPager.adapter = adapter
     }
 
+    private inner class MainActivityPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int {
+            return 9
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return TornadoInfoFragment()
+        }
+
+    }
 }
