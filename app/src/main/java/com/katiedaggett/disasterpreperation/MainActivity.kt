5d5189de852140914bbd8d6d179f52d7b88bef42
katherin97 <60371103+katherin97@users.noreply.github.com>
2020-04-26 20:40:55 -0500

    2020.04.27

--- app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
+++ app/src/main/java/com/katiedaggett/disasterpreperation/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
     lateinit var viewPager: ViewPager2
@@ -19,6 +21,38 @@ class MainActivity : FragmentActivity() {
 
         val adapter = MainActivityPagerAdapter(this)
         viewPager.adapter = adapter
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            if (position == 0) {
+                tab.text = "Tornado"
+            }
+            if (position == 1) {
+                tab.text = "Hurricane"
+            }
+            if (position == 2) {
+                tab.text = "Earthquake"
+            }
+            if (position == 3) {
+                tab.text = "Tsunami"
+            }
+            if (position == 4) {
+                tab.text = "Wildfire"
+            }
+            if (position == 5) {
+                tab.text = "Volcano"
+            }
+            if (position == 6) {
+                tab.text = "Flood"
+            }
+            if (position == 7) {
+                tab.text = "Blizzard"
+            }
+            if (position == 8) {
+                tab.text = "Drought"
+            }
+        }.attach()
     }
 
     private inner class MainActivityPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
@@ -27,8 +61,33 @@ class MainActivity : FragmentActivity() {
         }
 
         override fun createFragment(position: Int): Fragment {
-            return TornadoInfoFragment()
+            if (position == 0) {
+                return TornadoInfoFragment()
+            }
+            if (position == 1) {
+                return HurricaneInfoFragment()
+            }
+            if (position == 2) {
+                return EarthquakeInfoFragment()
+            }
+            if (position == 3) {
+                return TsunamiInfoFragment()
+            }
+            if (position == 4) {
+                return WildfireInfoFragment()
+            }
+            if (position == 5) {
+                return VolcanoInfoFragment()
+            }
+            if (position == 6) {
+                return FloodInfoFragment()
+            }
+            if (position == 7) {
+                return BlizzardInfoFragment()
+            }
+            return DroughtInfoFragment()
         }
 
     }
+
 }
--- app/src/main/java/com/katiedaggett/disasterpreperation/WildfireInfoFragment.kt
+++ app/src/main/java/com/katiedaggett/disasterpreperation/WildfireInfoFragment.kt
@@ -5,14 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class TornadoInfoFragment : Fragment() {
+class WildfireInfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tornado_info, container, false)
+        return inflater.inflate(R.layout.fragment_wildfire_info, container, false)
     }
 }
